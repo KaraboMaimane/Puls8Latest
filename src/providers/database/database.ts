243e1341beb23af8c539242e576b78af0cfb5216
unknown <janetzango@gmail.com>
2019-02-18 14:15:01 +0200

    Dj added

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -12,82 +12,83 @@ import firebase from 'firebase';
 
 @Injectable()
 export class DatabaseProvider {
-  allDjSArray=[];
+  allDjSArray = new Array();
   pic2;
 
-  constructor(public http: HttpClient,public alertCtrl: AlertController) {
+  constructor(public http: HttpClient, public alertCtrl: AlertController) {
     console.log('Hello DatabaseProvider Provider');
   }
 
-  // Register(fullname, email, password) {
-  //   return new Promise((accpt, rej) => {
-  //     firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
-  //       var user = firebase.auth().currentUser;
-  //       firebase.database().ref('Registration/' + user.uid).push({
-  //         fullname: fullname,
-  //         email: email,
-  //         role: "Audience",
-  //         userType: "user",
-  //         img: this.pic2,
-  //         key: user.uid
-  //       })
-  //       accpt("user registered")
+  Register(fullname, email, password) {
+    return new Promise((accpt, rej) => {
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+        var user = firebase.auth().currentUser;
+        firebase.database().ref('Registration/' + user.uid).push({
+          fullname: fullname,
+          email: email,
+          role: "Audience",
+          userType: "user",
+          img: this.pic2,
+          key: user.uid
+        })
+        accpt("user registered")
 
-  //     }, Error => {
-  //       let alert = this.alertCtrl.create({
-  //         title: 'Credentials Error',
-  //         message: Error.message,
-  //         buttons: ['Dismiss']
-  //       });
-  //       alert.present();
-  //     })
-  //   })
+      }, Error => {
+        let alert = this.alertCtrl.create({
+          title: 'Credentials Error',
+          message: Error.message,
+          buttons: ['Dismiss']
+        });
+        alert.present();
+      })
+    })
 
-  // }
+  }
 
-  // getAllDjs() {
-  //   return new Promise((accpt,rej) => {
-  //     firebase.database().ref('Registration/').on('value', (data: any) => {
-  //       var Djs = data.val();
-  //       var keys: any = Object.keys(Djs);
-  //       for (var i = 0; i < keys.length; i++) {
-  //         var x = keys[i];
-  //         var y = 'Registration/' + x;
-  //         firebase.database().ref(y).on('value', (data2: any) => {
-  //           var djInfomation = data2.val();
-  //           var keys2: any = Object.keys(djInfomation);
-  //           console.log(djInfomation)
-  //           console.log(keys2)
-  //           for (var j = 0; j < keys2.length; j++) {
-  //             var k = keys2[j];
-  //             console.log(k) 
-  //               let obj = {
-  //                 bio: djInfomation[k].bio,
-  //                 city: djInfomation[k].city,
-  //                 email: djInfomation[k].email,
-  //                 fullname: djInfomation[k].fullname,
-  //                 gender: djInfomation[k].gender,
-  //                 genre: djInfomation[k].genew,
-  //                 payment: djInfomation[k].payment,
-  //                 price: djInfomation[k].price,
-  //                 role: djInfomation[k].role,
-  //                 stagename: djInfomation[k].stagename,
-  //                 key: k
-  //               }
+  getAllDjs() {
+    return new Promise((accpt, rej) => {
+      firebase.database().ref('Registration/').on('value', (data: any) => {
+        var Djs = data.val();
+        var keys: any = Object.keys(Djs);
+        for (var i = 0; i < keys.length; i++) {
+          var x = keys[i];
+          var y = 'Registration/' + x;
+          firebase.database().ref(y).on('value', (data2: any) => {
+            var djInfomation = data2.val();
+            var keys2: any = Object.keys(djInfomation);
+            console.log(djInfomation)
+            console.log(keys2)
+            for (var j = 0; j < keys2.length; j++) {
+              var k = keys2[j];
+              console.log(k)
+              let obj = {
+                bio: djInfomation[k].bio,
+                city: djInfomation[k].city,
+                email: djInfomation[k].email,
+                fullname: djInfomation[k].fullname,
+                gender: djInfomation[k].gender,
+                genre: djInfomation[k].genew,
+                payment: djInfomation[k].payment,
+                price: djInfomation[k].price,
+                role: djInfomation[k].role,
+                img: djInfomation[k].img,
+                stagename: djInfomation[k].stagename,
+                key: k
+              }
 
-  //               if(obj.role == "Dj"){
-  //                 this.allDjSArray.push(obj);
-  //               console.log(obj)
-  //               }
-                
-  //           }
+              if (obj.role == "Dj") {
+                this.allDjSArray.push(obj);
+                console.log(obj)
+              }
 
-  //         })
-  //         accpt(this.allDjSArray)
-  //       }
-  //     })
-  //   })
-  // }
+            }
+
+          })
+          accpt(this.allDjSArray)
+        }
+      })
+    })
+  }
 
 
 
